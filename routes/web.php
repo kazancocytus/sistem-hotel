6ef3f271791a8844759f383c87ce71aa35ee916d
PramRadja <pramadhika7@gmail.com.com>
2024-01-23 15:13:54 +0700

    Uploaded

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/customer', [HomeController::class, 'home']); //Home Page
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,3 +34,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,26 @@ export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/views/homepage.blade.php',
     ],
 
     theme: {
+        colors: {
+            colorText: '#fff',
+            colorPrimary: '#FFFFEC',
+            colorSecondary: '#F1E4C3',
+            colorThird: '#C6A969',
+            colorgreen: '#597E52',
+            colorBg: '#04152d',
+        },
         extend: {
+            backgroundImage:{
+                hero: "url('/assets/heroimg.jpg')"
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                julius: ["Julius Sans One"],
+                josefin: ["'Josefin Sans', 'sans serif'"],
             },
         },
     },
