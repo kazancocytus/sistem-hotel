106761daeff0c8db0b5fdd52890e1e61ab7d5fe1
kazancocytus <zandirizkiawan165@gmail.com>
2024-01-29 11:24:06 +0700

    Uploaded

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; 
 
 class AdminController extends Controller
 {
@@ -29,5 +30,15 @@ class AdminController extends Controller
 
     public function AdminUser(){
         return view('admin.user');
+    }
+
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+ 
+        $request->session()->regenerateToken();
+        
+        return redirect()->route('login');
     }
 }
