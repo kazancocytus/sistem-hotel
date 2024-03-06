bf2182d52457a732a940351c9e968e7878ef9d40
kazancocytus <zandirizkiawan165@gmail.com>
2024-03-06 16:42:03 +0700

    Uplaoded

--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -116,7 +116,7 @@
                     <div class="card-header d-flex align-items-center justify-content-between pb-0 ">
                       <div class="card-title mb-0">
                         <h5 class="m-0 me-2">Reservation Statistics</h5>
-                        <small class=" fw-bold"> 546 Total Reservation This Year</small>
+                        <small class=" fw-bold"> {{ $count  }} Available Rooms</small>
                       </div>
                     </div>
                     <div class="card-body">
