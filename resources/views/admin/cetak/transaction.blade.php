a090790d19641ebb7c060be9092d1441e6241759
rallynah <ameliantiputri3@gmail.com>
2024-03-22 15:39:31 +0700

    Uploaded

--- resources/views/admin/cetak/transaction.blade.php
+++ resources/views/admin/cetak/transaction.blade.php
@@ -19,36 +19,34 @@
         <div class="col-lg-12 py-3">
             <div class="card shadow">
                 <div class="table-responsive rounded-lg text-nowrap">
-                  <table class="table bg-colorPrimary">
-                    <thead class=" bg-colorSecondary">
-                      <tr class="font-josefin">
-                        <th>No Reservation</th>
-                        <th>Customer Name</th>
-                        <th>Phone</th>
-                        <th>Suites Room</th>
-                        <th>Deluxe Room</th>
-                        <th>Standart Room</th>
-                        <th>Check-in</th>
-                        <th>Check-out</th>
-                        <th>Price</th>
-                      </tr>
-                    </thead>
-                    <tbody class="table-border-bottom-0">
-                      @foreach($transaction as $data)
-                      <tr>
-                        <td>{{ $data->no_reservation }} </td>
-                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>{{ $data->name }}</strong></td>
-                        <td>{{ $data->phone }}</td>
-                        <td>{{ $data->suite }}</td>
-                        <td>{{ $data->deluxe }}</td>
-                        <td>{{ $data->standart }}</td>
-                        <td>{{ $data->check_in }}</td>
-                        <td>{{ $data->check_out }}</td>
-                        <td> ${{ $data->price }}</td>
-                      </tr>
-                      @endforeach
-                    </tbody>
-                  </table>
+                  <table class="table bg-colorPrimary static" align="center" border="1px" style="width: 95%;">
+                        <thead class=" bg-colorSecondary">
+                            <tr class="font-josefin">
+                                <th>No Reservation</th>
+                                <th>Customer Name</th>
+                                <th>Phone</th>
+                                <th>Suites Room</th>
+                                <th>Deluxe Room</th>
+                                <th>Standart Room</th>
+                                <th>Check-in</th>
+                                <th>Check-out</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                            @foreach($transaction as $data)
+                            <tr>
+                                <td>{{ $data->no_reservation }} </td>
+                                <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>{{ $data->name }}</strong></td>
+                                <td>{{ $data->phone }}</td>
+                                <td>{{ $data->suite }}</td>
+                                <td>{{ $data->deluxe }}</td>
+                                <td>{{ $data->standart }}</td>
+                                <td>{{ $data->check_in }}</td>
+                                <td>{{ $data->check_out }}</td>
+                                <td> ${{ $data->price }}</td>
+                            </tr>
+                            @endforeach
+                    </table>
                 </div>
             </div>
         </div>          
